46ec2344e5f29916a4bced101078884a0a3cb43a
LukeCaruzo <schmidt.lucas@googlemail.com>
2018-10-17 11:47:50 +0200

    uses routes

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,24 +6,17 @@ import play.api._
 import play.api.mvc._
 import views.WebInterface
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  val student = "Lucas/Bilal"
+  val game = new Game
+  val webInterface = new WebInterface(student, game)
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    val student = "Lucas/Bilal"
-    val game = new Game
+  def index() = Action { // implicit request: Request[AnyContent] =>
+    Ok(webInterface.initialize)
+  }
 
-    Ok(new WebInterface(student, game).initialize)
+  def action(input: Char) = Action {
+    Ok(webInterface.action(input))
   }
 }
