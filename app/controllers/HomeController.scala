4eb7a3aa0b81d8ae69e0ef9691f8947285aa5c89
LukeCaruzo <schmidt.lucas@googlemail.com>
2018-11-28 09:19:09 +0100

    websockets init

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,9 +1,13 @@
 package controllers
 
+import akka.actor.{Actor, ActorRef, Props}
+import com.google.gson.Gson
 import de.htwg.se.twothousandfortyeight.model.gameModel.gameBaseImpl.Game
 import javax.inject._
+import play.api.libs.streams.ActorFlow
 import play.api.mvc._
-import com.google.gson.Gson
+
+import scala.swing.Reactor
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
@@ -20,8 +24,45 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.game())
   }
 
-  def gameToJson() = Action {
+  def gameToJsonAjax() = Action {
     Ok(gson.toJson(game))
+  }
+
+  // TODO: Finish this mess
+  def gameToJsonWebSocket() = WebSocket.accept[String, String] {
+    request {
+      ActorFlow.actorRef { out =>
+        println("Connect received")
+        WebSocketActorFactory.create(out)
+      }
+    }
+
+    object WebSocketActorFactory {
+      def create(out: ActorRef) = {
+        Props(new WebSocketActor(out))
+      }
+    }
+
+    class WebSocketActor(out: ActorRef) extends Actor with Reactor {
+      listenTo(gameController)
+
+      def receive = {
+        case msg: String =>
+          out ! (gameController.toJson.toString)
+          println("Sent Json to Client" + msg)
+      }
+
+      reactions += {
+        case event: GridSizeChanged => sendJsonToClient
+        case event: CellChanged => sendJsonToClient
+        case event: CandidatesChanged => sendJsonToClient
+      }
+
+      def sendJsonToClient = {
+        println("Received event from Controller")
+        out ! (gameController.toJson.toString)
+      }
+    }
   }
 
   def up() = Action {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -69,10 +69,10 @@ function clickListener() {
     });
 }
 
-function init() {
+function ajax() {
     $.ajax({
         method: "GET",
-        url: "/toJson",
+        url: "/toJsonAjax",
         dataType: "json",
 
         success: function (result) {
@@ -82,6 +82,36 @@ function init() {
     });
 }
 
+// TODO: Finish this mess
+function webSocket() {
+    var websocket = new WebSocket("ws://localhost:9000/websocket");
+    websocket.setTimeout
+
+    websocket.onopen = function (event) {
+        console.log("Connected to Websocket");
+    }
+
+    websocket.onclose = function () {
+        console.log('Connection with Websocket Closed!');
+    };
+
+    websocket.onerror = function (error) {
+        console.log('Error in Websocket Occured: ' + error);
+    };
+
+    websocket.onmessage = function (e) {
+        if (typeof e.data === "string") {
+            var json = JSON.parse(e.data);
+            var cells = json.grid.cells;
+            grid.fill(cells);
+            updateGrid(grid);
+            registerClickListener();
+        }
+
+    };
+}
+
 $(document).ready(function () {
-    init();
+    ajax();
+    // webSocket();
 });
